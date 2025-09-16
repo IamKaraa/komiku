f2371830db68dac6491e1dedf36805f08c1bdae2
Intan Nur Laila <149084934+Innulaa@users.noreply.github.com>
2025-09-16 20:26:34 +0700

    Update 2025_09_15_154258_users.php

--- database/migrations/2025_09_15_154258_users.php
+++ database/migrations/2025_09_15_154258_users.php
@@ -11,31 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->string('status');
-            $table->date('birth_date');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('favorite_genres', function(Blueprint $table){
-            $table->foreignId('user_id')->nullable()->index();
-            
-            $table->timestamps();
-        });
-    }
+        
 
     /**
      * Reverse the migrations.
